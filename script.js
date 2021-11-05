c5b9d18709cf37c768bc86860dfcf2c9e8089229
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2021-11-05 17:36:13 +1100

    added jpg

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ window.onscroll = function() {
   if (window.pageYOffset > 0) {
  var opac = (window.pageYOffset / nystories);
     console.log(opac);
-  document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(Assets/NNRBanner_1) no-repeat";
+  document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(https://github.com/niruvibes/NotNecessarily/blob/main/Assets/NNRBanner_1.png) no-repeat";
   }
 }
