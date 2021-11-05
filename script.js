6a7281cfe87c30fdc50b1087a28a31377d26bc41
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2021-11-05 17:33:54 +1100

    updating image

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ window.onscroll = function() {
   if (window.pageYOffset > 0) {
  var opac = (window.pageYOffset / nystories);
     console.log(opac);
-  document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/times-square-perspective.jpg) no-repeat";
+  document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(Assets/NNRBanner_1) no-repeat";
   }
 }
