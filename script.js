78c4cd5bc3d703b80e13c0511ab5d33314be7496
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2021-11-08 08:37:50 +1100

    commit to main

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var nystories = document.querySelector("p").offsetTop;
+var notNecessarily = document.querySelector("p").offsetTop;
 window.onscroll = function() {
   if (window.pageYOffset > 0) {
- var opac = (window.pageYOffset / nystories);
+ var opac = (window.pageYOffset / notNecessarily);
     console.log(opac);
-  document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(https://github.com/niruvibes/NotNecessarily/blob/main/Assets/NNRBanner_1.png) no-repeat";
+  document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(Assets/logo1500994.png) no-repeat";
   }
 }
